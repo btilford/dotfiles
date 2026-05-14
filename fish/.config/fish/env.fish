912ee358e6283e38fce230ad0509706f840450bd
btilford <248725+btilford@users.noreply.github.com>
2026-05-14 10:41:34 -0600

    fixing some mize and starship issues

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -9,22 +9,13 @@ set -x VISUAL 'nvim-qt'
 
 
 set -x M2_HOME $HOME/.m2
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
 
 set -gx --prepend PATH $HOME/.cargo/bin
 set -gx --prepend PATH $HOME/.local/bin
+# mise shims last so they win over homebrew and other system tools
+if not contains $HOME/.local/share/mise/shims $PATH
+    set -gx --prepend PATH $HOME/.local/share/mise/shims
+end
 # fzf key bindings are interactive-only; guard so Claude/scripts don't error
 if status is-interactive
     fzf --fish | source
